1ba25e833c704cb448e1cbe0e3cb9bcf4e29ae3a
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-02-05 14:47:14 +0100

    start working on Cat class

--- src/entities/Furry.js
+++ src/entities/Furry.js
@@ -2,3 +2,79 @@
 
 import Phaser from "phaser";
 
+export default class Furry extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture = 'cat') {
+        super(scene, x, y, 'player');
+
+        // Physics setup
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        this.body.setSize(this.width/4, this.height/3);
+        this.body.setOffset(19, 25);
+
+        this.facing = 'down';
+
+        this.createAnimations(scene);
+        }
+
+    createAnimations(scene) {
+        scene.anims.create({
+        key: 'cat-idle-down',
+        frames: scene.anims.generateFrameNumbers('cat', {start: 0, end: 2}),
+        frameRate: 1,
+        repeat: -1,
+    });
+
+    scene.anims.create({
+        key: 'cat-idle-up',
+        frames: scene.anims.generateFrameNumbers('cat', {start: 3, end: 5}),
+        frameRate: 1,
+        repeat: -1,
+    });
+
+    scene.anims.create({
+        key: 'cat-idle-right',
+        frames: scene.anims.generateFrameNumbers('cat', {start: 6, end: 8}),
+        frameRate: 1,
+        repeat: -1,
+    });
+
+    scene.anims.create({
+        key: 'cat-idle-left',
+        frames: scene.anims.generateFrameNumbers('cat', {start: 9, end: 11}),
+        frameRate: 1,
+        repeat: -1,
+    });
+
+    scene.anims.create({
+        key: 'cat-walk-down',
+        frames: scene.anims.generateFrameNumbers('cat', {start: 12, end: 14}),
+        frameRate: 1,
+        repeat: -1,
+    });
+
+    scene.anims.create({
+        key: 'cat-walk-up',
+        frames: scene.anims.generateFrameNumbers('cat', {start: 15, end: 17}),
+        frameRate: 1,
+        repeat: -1,
+    });
+
+    scene.anims.create({
+        key: 'cat-walk-right',
+        frames: scene.anims.generateFrameNumbers('cat', {start: 18, end: 20}),
+        frameRate: 1,
+        repeat: -1,
+    });
+
+    scene.anims.create({
+        key: 'cat-walk-left',
+        frames: scene.anims.generateFrameNumbers('cat', {start: 21, end: 23}),
+        frameRate: 1,
+        repeat: -1,
+    });
+
+    }
+
+}
